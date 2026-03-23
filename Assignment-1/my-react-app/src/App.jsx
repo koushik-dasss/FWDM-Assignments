5e29e761ea241e08913b3744f6ff6f90888d281d
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-03-24 00:13:15 +0530

    default jsx and styling css files

---
+++ Assignment-1/my-react-app/src/App.jsx
@@ -0,0 +1,72 @@
+// // Question -1 (only App.jsx code in this question)
+
+// function App(){
+//   return(
+//     <div>
+//       <h1>Hello World from React using Vite</h1>
+//     </div>
+//   );
+// }
+// export default App;
+
+//Question-2
+
+// import Counter from "./Question-2_Counter";
+
+// function App(){
+//   return(
+//     <div>
+//       <h1>Counter Application</h1>
+//       <Counter/>
+//     </div>    
+//   );
+// }
+// export default App;
+
+//Question-3
+
+// import ThemeToggle from "./Question-3_ThemeToggle";
+
+// function App(){
+//   return(
+//     <div>
+//       <ThemeToggle/>
+//     </div>
+//   );
+// }
+// export default App;
+
+// Question-4
+// import Greeting from "./Question-4_Greeting";
+
+// function App(){
+//   return(
+//     <div>
+//       <Greeting name="Koushik" />
+//       <Greeting name="Roman" />
+//       <Greeting name="Dean" />
+//     </div>
+//   );
+// }
+// export default App;
+
+// Question-5
+import { BrowserRouter , Routes , Route , Link} from "react-router-dom";
+import Home from "./Question-5_Home";
+import About from "./Question-5_About";
+
+function App(){
+  return(
+    <BrowserRouter>
+    <nav>
+      <Link to="/">Home</Link> | 
+      <Link to="/about">About</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>} />
+    </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
