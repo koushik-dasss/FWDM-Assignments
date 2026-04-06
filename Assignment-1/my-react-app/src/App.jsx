fd51149d948a7a75904adc541279c88af903c5cc
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-04-06 19:00:42 +0530

    comment modif

--- Assignment-1/my-react-app/src/App.jsx
+++ Assignment-1/my-react-app/src/App.jsx
@@ -51,22 +51,22 @@
 // export default App;
 
 // Question-5
-import { BrowserRouter , Routes , Route , Link} from "react-router-dom";
-import Home from "./Question-5_Home";
-import About from "./Question-5_About";
+// import { BrowserRouter , Routes , Route , Link} from "react-router-dom";
+// import Home from "./Question-5_Home";
+// import About from "./Question-5_About";
 
-function App(){
-  return(
-    <BrowserRouter>
-    <nav>
-      <Link to="/">Home</Link> | 
-      <Link to="/about">About</Link>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>} />
-    </Routes>
-    </BrowserRouter>
-  );
-}
-export default App;
+// function App(){
+//   return(
+//     <BrowserRouter>
+//     <nav>
+//       <Link to="/">Home</Link> | 
+//       <Link to="/about">About</Link>
+//     </nav>
+//     <Routes>
+//       <Route path="/" element={<Home/>}/>
+//       <Route path="/about" element={<About/>} />
+//     </Routes>
+//     </BrowserRouter>
+//   );
+// }
+// export default App;
