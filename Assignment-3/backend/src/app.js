0844c352e05e39ac1e2f6608c768482ae220f7fc
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-04-07 00:20:55 +0530

    development of Express application to handle API Routes for a product post system

--- Assignment-3/backend/src/app.js
+++ Assignment-3/backend/src/app.js
@@ -1,12 +1,14 @@
 // QS. 6 
-
 const express = require('express');
+const cors = require('cors');
 const initDatabase = require("./db/init");
 const postsRoutes = require("./routes/posts");
 const app = express();
+// middleware
+app.use(cors); 
 app.use(express.json()); // body parser (middleware)
 initDatabase(); // DB connection
-app.use(postsRoutes); // routes connected 
+app.use("/api",postsRoutes); // routes connected , if posts.js uses express.Router()
 app.listen(3000 , () => {
     console.log("Server running on port 3000"); // server start display message 
 });
