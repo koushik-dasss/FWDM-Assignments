57599c5404cd4af21e9694e9b946585c6a61b19c
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-05-10 00:30:39 +0530

    Home page

---
+++ Minor Project/library-management/frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Home(){
+  return (
+    <div className="page">
+      <h1>Koushik's Library Management System</h1>
+      <p>
+        Your Gateway to Organized Knowledge and Seamless Book Management
+      </p>
+    </div>
+  );
+}
+export default Home;
