587f6c9da0145d5513ba7ee00276c40801225709
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-05-10 00:33:33 +0530

    update page for placeholder

---
+++ Minor Project/library-management/frontend/src/pages/UpdateBookPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+function UpdateBookPage() {
+  return (
+    <div className="page">
+      <h1>Update Book Feature</h1>
+      <p>
+        Update functionality can be implemented here.
+      </p>
+    </div>
+  );
+}
+export default UpdateBookPage;
