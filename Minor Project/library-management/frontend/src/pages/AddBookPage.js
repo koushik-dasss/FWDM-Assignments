b27664fb91a05795a8d73eb798a3ed0ec55ac517
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-05-10 00:32:15 +0530

    Books addition page(the interaction with user page)

---
+++ Minor Project/library-management/frontend/src/pages/AddBookPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AddBookForm from "../components/AddBookForm";
+function AddBookPage() {
+    return (
+     <div>
+       <AddBookForm />
+     </div>
+    );
+}
+export default AddBookPage;
