c1541cf37a982b728b5038277bf24344c714e822
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-05-16 13:37:06 +0530

    all components combination

---
+++ Assignment-4/product-store/frontend/src/App.js
@@ -0,0 +1,33 @@
+// Question-1
+
+// import LoginForm from "./components/LoginForm";
+// import RegisterForm from "./components/RegisterForm";
+// import ProductSearchForm from "./components/ProductSearchForm";
+// function App() {
+//   return (
+//     <div>
+//       <LoginForm />
+//       <hr />
+//       <RegisterForm />
+//       <hr />
+//       <ProductSearchForm />
+//     </div>
+//   );
+// }
+// export default App;
+
+// Question-2
+
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Products from "./components/Products";
+function App() {
+  return (
+    <div>
+      <Register />
+      <Login />
+      <Products />
+    </div>
+  );
+}
+export default App;
