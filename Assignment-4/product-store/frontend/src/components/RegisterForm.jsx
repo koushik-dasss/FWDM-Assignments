3044a34acdf198cd0e5281e17e7774c3e1ae0fe4
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-06-07 18:48:30 +0530

    Register Form modified

--- Assignment-4/product-store/frontend/src/components/RegisterForm.jsx
+++ Assignment-4/product-store/frontend/src/components/RegisterForm.jsx
@@ -1,26 +1,46 @@
 import React,{ useState } from "react";
-function LoginForm(){
+function RegisterForm(){
+    const[username,setUsername] = useState("");
     const[email,setEmail] = useState("");
     const[password,setPassword] = useState("");
+    const[confirmPassword,setConfirmPassword] = useState("");
     const[errors,setErrors] = useState({});
     const handleSubmit = (e) => {
         e.preventDefault();
         let newErrors = {};
+        if(!username){
+            newErrors.username = "Username required";
+        }
         if(!email){
-            newErrors.email = "Email is required";
+            newErrors.email = "Email required";
         }
         if(!password){
-            newErrors.password = "Password is required";
+            newErrors.password = "Password required";
+        }
+        if(password !== confirmPassword){
+            newErrors.confirmPassword = "Passwords do not match";
         }
         setErrors(newErrors);
         if(Object.keys(newErrors).length === 0){
-            alert("Login Successful");
+            alert("Registration Successful");
         }
     };
     return(
         <div>
-            <h2>Login Form</h2>
+            <h2>Register Form</h2>
             <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username:</label>
+                    <br />
+                    <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <p style={{color:"red"}}>
+                        {errors.username}
+                    </p>
+                </div>
                 <div>
                     <label>Email:</label>
                     <br />
@@ -45,11 +65,23 @@ function LoginForm(){
                         {errors.password}
                     </p>
                 </div>
+                <div>
+                    <label>Confirm Password:</label>
+                    <br />
+                    <input
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    <p style={{color:"red"}}>
+                        {errors.confirmPassword}
+                    </p>
+                </div>
                 <button type="submit">
-                    Login
+                    Register
                 </button>
             </form>
         </div>
     );
 }
-export default LoginForm;
+export default RegisterForm;
